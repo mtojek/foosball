d1b42bbc3e0708b4f2d400b9e7c268235a095f23
Marcin Tojek <tojek.m@gmail.com>
2018-02-07 01:22:04 +0100

    Add Firebase support

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import firebase from 'firebase';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyC_r7-a63XcvHkbet3UnsqZaP0DKIJpLqE',
+  authDomain: 'foosball-77b90.firebaseapp.com',
+  databaseURL: 'https://foosball-77b90.firebaseio.com',
+  projectId: 'foosball-77b90',
+  storageBucket: 'foosball-77b90.appspot.com',
+  messagingSenderId: '642361302451',
+};
+
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
