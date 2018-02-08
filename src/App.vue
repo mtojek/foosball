031e36e52b18b7f0d1d75dc4587b732f349ae3b4
Marcin Tojek <tojek.m@gmail.com>
2018-02-08 14:27:25 +0100

    Use bulma for templating
    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#"><b>foosball</b></a>
+          <div class="navbar-burger burger" data-target="navMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="navbar-menu" id="navMenu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="#">Matches</a>
+            <a class="navbar-item" href="#">Players</a>
+          </div>
+          <div class="navbar-end">
+            <a class="navbar-item" href="#">Add match</a>
+            <a class="navbar-item" href="#">Sign in</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -9,15 +32,30 @@
 export default {
   name: 'App',
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(($el) => {
+      $el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = $el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  @import '../node_modules/bulma/bulma.sass'
+
 </style>
