b987e06442cd72bf390969f5939ddedbf639007f
Marcin Tojek <tojek.m@gmail.com>
2018-02-09 00:14:07 +0100

    Move NavBar to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="#">
-            <span class="app-name">foosball</span>
-          </a>
-          <div class="navbar-burger burger" data-target="navMenu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div class="navbar-menu" id="navMenu">
-          <div class="navbar-start">
-            <a class="navbar-item" href="#">Matches</a>
-            <a class="navbar-item" href="#">Players</a>
-          </div>
-          <div class="navbar-end">
-            <a class="navbar-item" href="#">Add match</a>
-            <a class="navbar-item" href="#">Sign in</a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <nav-bar/>
     <div class="container">
       <router-view/>
     </div>
@@ -31,7 +8,12 @@
 </template>
 
 <script>
+import NavBar from '@/components/NavBar';
+
 export default {
+  components: {
+    NavBar,
+  },
   name: 'App',
 };
 
@@ -60,6 +42,4 @@ document.addEventListener('DOMContentLoaded', () => {
 <style lang="sass">
   @import '../node_modules/bulma/bulma.sass'
 
-  span.app-name
-    font-weight: bold
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+          <span class="app-name">foosball</span>
+        </a>
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div class="navbar-menu" id="navMenu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="#">Matches</a>
+          <a class="navbar-item" href="#">Players</a>
+        </div>
+        <div class="navbar-end">
+          <a class="navbar-item" href="#">Add match</a>
+          <a class="navbar-item" href="#">Sign in</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style lang="sass" scoped>
+  span.app-name
+    font-weight: bold
+</style>
