d4cfe86f59b91608418b70d77edecf2b70bc7a69
Marcin Tojek <tojek.m@gmail.com>
2018-02-22 00:46:49 +0100

    Add HTML to Leaderboard and Recent Matches

--- src/components/home/RecentMatches.vue
+++ src/components/home/RecentMatches.vue
@@ -3,7 +3,43 @@
     <div class="section">
       <h1 class="title">Recent Matches</h1>
       <div class="body">
-        TODO
+        <table class="table">
+          <tbody>
+            <tr>
+              <td class="name">John Smith</td>
+              <td class="score" rowspan="2">
+                3 : 0
+              </td>
+              <td class="name">Jerry Ford</td>
+            </tr>
+            <tr>
+              <td class="name">Bevis Titus</td>
+              <td class="name">Nash Nicky</td>
+            </tr>
+            <tr>
+              <td class="name">John Smith</td>
+              <td class="score" rowspan="2">
+                1 : 2
+              </td>
+              <td class="name">Jerry Ford</td>
+            </tr>
+            <tr>
+              <td class="name">Bevis Titus</td>
+              <td class="name">Nash Nicky</td>
+            </tr>
+            <tr>
+              <td class="name">John Smith</td>
+              <td class="score" rowspan="2">
+                2 : 2
+              </td>
+              <td class="name">Jerry Ford</td>
+            </tr>
+            <tr>
+              <td class="name">Bevis Titus</td>
+              <td class="name">Nash Nicky</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -15,5 +51,17 @@ export default {
 };
 </script>
 
-<style>
+<style lang="sass" scoped>
+  td.name
+    text-align: center
+  td.score
+    font-size: 1.5rem
+    font-weight: bold
+    vertical-align: middle
+  tr:nth-child(odd)
+    td.name
+      border: none
+  tr:nth-last-child(2)
+    td.score
+      border: none
 </style>
