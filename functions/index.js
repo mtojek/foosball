e751d228df5785e4ebf68665ce04d19c41cec97f
Marcin Tojek <tojek.m@gmail.com>
2018-03-08 03:14:19 +0100

    Implement onWrite handler for matches

--- functions/index.js
+++ functions/index.js
@@ -55,7 +55,6 @@ exports.onCreateMatch = functions.database.ref('matches/{uid}')
   .onCreate(event => {
     const match = event.data.val();
 
-    // Team A
     return Promise.all([
       database.ref('/leaderboard/' + match.team_a.first)
         .transaction((stats) => updateTeamAPlayer(stats, match, 'create')),
@@ -91,4 +90,45 @@ exports.onDeleteMatch = functions.database.ref('matches/{uid}')
     });
   });
 
-// FIXME: onUpdate can't be handled right now.
+function calculatePlayerStats(team, oldStats, match) {
+  let stats = oldStats;
+
+  if (stats === undefined) {
+    stats = createNewLeader();
+  }
+
+  stats.played = stats.played + 1;
+  stats.drawn = stats.drawn + teams_drawn(match);
+
+  if (team === 'A') {
+    stats.won = stats.won + team_a_won(match);
+    stats.lost = stats.lost + (!team_a_won(match) && !teams_drawn(match));
+    stats.points = stats.points + match.team_a.score;
+  } else {
+    stats.won = stats.won + (!team_a_won(match) && !teams_drawn(match));
+    stats.lost = stats.lost + team_a_won(match);
+    stats.points = stats.points + match.team_b.score;
+  }
+  return stats;
+}
+
+exports.onUpdateMatch = functions.database.ref('matches/{uid}')
+  .onWrite(event => {
+    const leaders = {};
+    const matchesRef = event.data.ref.parent;
+    return matchesRef.once('value', snapshot => {
+      const matches = snapshot.val();
+      for (let key in matches) {
+        const match = matches[key];
+        leaders[match.team_a.first] = calculatePlayerStats('A', leaders[match.team_a.first], match);
+        leaders[match.team_a.second] = calculatePlayerStats('A', leaders[match.team_a.second], match);
+        leaders[match.team_b.first] = calculatePlayerStats('B', leaders[match.team_b.first], match);
+        leaders[match.team_b.second] = calculatePlayerStats('B', leaders[match.team_b.second], match);
+      }
+    }).then(() => {
+      return database.ref('leaderboard2').update(leaders);
+    }).then(() => {
+      console.log('Leaderboard has been updated.');
+      return true;
+    });
+  });
